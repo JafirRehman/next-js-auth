ce1cf3b1aa809639e47e4e801cb03965ea62de33
JafirRehman <mjafirrehman@gmail.com>
2024-07-08 06:30:58 -0700

    adding 2 project

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-black text-white flex justify-center items-center">Home </div>
+  );
+}
